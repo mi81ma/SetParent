fff9c0b3a93a29dc1fc838d7c19ee0ca8f751336
mi81ma <mi81ma@gmail.com>
2020-04-03 17:27:54 +0800

    initial commit

---
+++ Assets/CreateCard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCard : MonoBehaviour
+{
+    // 生成したいオブジェクトを用意(窓口)
+    public GameObject cardImage;
+    
+    // 親を用意(窓口)
+    [SerializeField] Transform cardParent;
+    
+    void Start()
+    {
+        /*
+        // 1.オブジェクトの生成
+        GameObject cardObj = Instantiate(cardImage);
+        // 2.親の設定
+        cardObj.transform.SetParent(cardParent, false);
+        */
+        
+        //別解
+        Instantiate(cardImage, cardParent);
+    }
+}
