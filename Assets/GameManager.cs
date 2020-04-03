9d07f4dc206e2b5d7b032098c2bbef3d80283cfa
mi81ma <mi81ma@gmail.com>
2020-04-03 19:36:15 +0800

    Change UI Value

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,19 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // UnityのUIを扱うには、UnityEngine.UIをusing
 
 public class GameManager : MonoBehaviour
 {
 
-    // 1. 表示/非表示したいオブジェクトを用意(窓口)
-    [SerializeField] GameObject panel;
+    // 1. 取得： 変更したいTextを用意 (窓口)
+    [SerializeField] Text viewText;
     void Start()
     {
-        // 2. 非表示
-        // panel.SetActive(false);
-
-        // 2'. 表示
-        panel.SetActive(true);
+        // 2. 変更: UIの値を変更する
+        viewText.text = "Test UI";
 
     }
 }
