d5acec477d8303400e444623e64a1dff1c2d3330
mi81ma <mi81ma@gmail.com>
2020-04-03 20:10:22 +0800

    Drag and Drop

---
+++ Assets/CardMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;  // マウスの動きを読み取る
+
+public class CardMovement : MonoBehaviour, IDragHandler
+{
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position; // Left Hand is your position = Right Hand is Mouse position
+    }
+
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(CountDown()); // Set CountDown function by StartCoroutine()
+    }
+
+    // Make some function
+    IEnumerator CountDown()
+    {
+       Debug.Log("CountDown Start"); // Log out
+       while (true)
+       {
+            yield return new WaitForSeconds(1); // Wait 1 second
+            Debug.Log("After 1 second"); // Log out
+       }
+    }
+}
